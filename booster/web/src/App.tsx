c101d7000628d6180d1b2cd03fd0150f3f8a4a58
Lucas <l.alves001@gmail.com>
2020-06-08 23:51:28 -0300

    Day 3

--- booster/web/src/App.tsx
+++ booster/web/src/App.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Routes from "./routes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React</h1>
-      </header>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
